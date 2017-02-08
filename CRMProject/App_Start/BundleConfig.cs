902ead77d693f422d99256b077ac793f7884abd3
prabhata kumar maharana <maharanakumar2@outlook.com>
2017-02-08 18:13:48 +0530

    Add project files.

---
+++ CRMProject/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace CRMProject.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/Script").Include
+                (
+                "~/Contents/Scripts/jquery-3.1.1.js",
+                "~/Contents/Scripts/bootstrap.min.js"
+                )
+                );
+            bundles.Add(new StyleBundle("~/bundles/Style").Include
+                (
+                "~/Contents/css/bootstrap.min.css"
+                )
+                );
+        }
+    }
+}
---
+++ CRMProject/Controllers/MySearchAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRMProject.Controllers
+{
+    public class MySearchAppController : Controller
+    {
+        // GET: MySearchApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
